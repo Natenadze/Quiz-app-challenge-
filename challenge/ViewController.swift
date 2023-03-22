5c2b44a85917ebd4bd2ed7931ded088c3eb5c140
davit <davit.natenadze@gmail.com>
2023-03-22 18:35:32 +0100

    Initial Commit

---
+++ challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge
+//
+//  Created by Davit Natenadze on 22.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
